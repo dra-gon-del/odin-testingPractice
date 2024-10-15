e153fb8c0ddc5bbf752d2c55b1b239308eb6baef
A.Westwood <personal.email.aw1@gmail.com>
2024-10-15 13:20:52 +0100

    07. write test for capitalise and reverse string. write funct for capitalise.

--- tests/index.test.js
+++ tests/index.test.js
@@ -2,15 +2,30 @@ const {capitalize} = require('../src/index')
 
 describe('capitalize', () => {
     test('capitalizes the first character of a string', () => {
-        expect(capitalize('hello world').toBe('Hello world'));
+        expect(capitalize('hello world')).toBe('Hello world');
     })
     test('capitalizes a single character', () => {
-        expect(capitalize('a').toBe('A'));
+        expect(capitalize('a')).toBe('A');
     })
     test('works with empty string', () => {
-        expect(capitalize('').toBe(''));
+        expect(capitalize('')).toBe('');
     })
     test('does not modify already capitalized string', () => {
-        expect(capitalize('Hello').toBe('Hello'));
+        expect(capitalize('Hello')).toBe('Hello');
+    })
+});
+
+describe('reverseString', () => {
+    test('reverses a common string', () => {
+        expect(capitalize('hello')).toBe('olleh');
+    })
+    test('capitalizes a single character', () => {
+        expect(capitalize('a')).toBe('a');
+    })
+    test('works with empty string', () => {
+        expect(capitalize('')).toBe('');
+    })
+    test('reverses correctly a string with punctuation', () => {
+        expect(capitalize('Hello!')).toBe('olleH!');
     })
 });
